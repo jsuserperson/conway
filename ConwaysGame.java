7f45152bc7bc7d23f5127a2de9f5f0be0ca1f6ad
jsuserperson <jonah.shen42@gmail.com>
2022-12-12 14:52:23 -0800

    w17d1

--- ConwaysGame.java
+++ ConwaysGame.java
@@ -3,37 +3,35 @@ import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
-import java.util.ArrayList;
+import org.w3c.dom.events.MouseEvent;
 
-public class ConwaysGame extends JPanel {
+public class ConwaysGame extends JPanel{
     private JFrame frame;
-    private int winWidth = 300;
-    private int winHeight = 200;
+    private boolean started = false;
 
-    private int width = 1;
-    private int height = 1;
-    boolean[][] grid;
-
-    private int inputInt;
+    private int width = 10;
+    private int height = 10;
+    boolean[][] grid = new boolean[height][width];
 
     JLabel inputLabel = new JLabel("width: ");
 
     NumberFormat inputFormatter = NumberFormat.getIntegerInstance();
     JFormattedTextField input = new JFormattedTextField(inputFormatter);
-    
 
     public ConwaysGame() {
-        JFrame frame = new JFrame("Conway's Game of Life");
+        frame = new JFrame("Conway's Game of Life");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
         this.add(inputLabel);
         input.setPreferredSize(new Dimension(50, 20));
         this.add(input);
         input.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                inputInt = Integer.parseInt(input.getText());
-                System.out.println(inputLabel.getText() + inputInt);
+                int inputInt = Integer.parseInt(input.getText());
+                // System.out.println(inputLabel.getText() + inputInt);
                 if (inputLabel.getText().equals("width: ")) {
                     width = inputInt;
                     input.setText("");
@@ -44,32 +42,41 @@ public class ConwaysGame extends JPanel {
                 }
             }
         });
-        frame.setSize(300, 200);
-        frame.setVisible(true);
+
     }
 
     public void gameStart(int width, int height) {
         grid = new boolean[height][width];
         for (int h = 0; h < height; h++) {
-            String row = "";
+            // String row = "";
             for (int w = 0; w < width; w++) {
-                row += grid[h][w] + " ";
+                // row += grid[h][w] + " ";
             }
-            System.out.println(row);
+            // System.out.println(row);
+            this.remove(inputLabel);
+            this.remove(input);
+            frame.setSize(width * 10 + 30, height * 10 + 50);
+            started = true;
         }
     }
 
+    public void MouseClicked(MouseEvent e) {
+        
+    }
+
     public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(1));
-        for (int h = 0; h < height; h++) {
-            for (int w = 0; w < width; w++) {
-                if (grid[h][w]) {
+        g.setColor(getBackground());
+        g.fillRect(0, 0, this.getSize().width, this.getSize().height);
+        if (started) {
+            for (int h = 0; h < height; h++) {
+                for (int w = 0; w < width; w++) {
+                    if (!grid[h][w]) {
+                        g.setColor(Color.white);
+                    }
+                    g.fillRect(w * 10, h * 10, 10, 10);
                     g.setColor(Color.black);
-                } else {
-                    g.setColor(Color.white);
+                    g.drawRect(w * 10, h * 10, 10, 10);
                 }
-                g.fillRect(w * 10, h * 10, 10, 10);
             }
         }
     }
