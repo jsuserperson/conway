514e0221d86f563443208e74b54b11ecfcf0a990
jsuserperson <jonah.shen42@gmail.com>
2023-01-04 15:05:19 -0800

    s2w1d1

--- ConwaysGame.java
+++ ConwaysGame.java
@@ -1,13 +1,11 @@
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.text.NumberFormat;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 import javax.swing.*;
-import org.w3c.dom.events.MouseEvent;
 
-public class ConwaysGame extends JPanel{
+public class ConwaysGame extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
     private JFrame frame;
-    private boolean started = false;
+    private boolean setup = false;
 
     private int width = 10;
     private int height = 10;
@@ -24,6 +22,9 @@ public class ConwaysGame extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
         frame.setSize(300, 200);
+        frame.setResizable(false);
+        addMouseListener(this);
+        addMouseMotionListener(this);
         frame.setVisible(true);
         this.add(inputLabel);
         input.setPreferredSize(new Dimension(50, 20));
@@ -55,19 +56,16 @@ public class ConwaysGame extends JPanel{
             // System.out.println(row);
             this.remove(inputLabel);
             this.remove(input);
-            frame.setSize(width * 10 + 30, height * 10 + 50);
-            started = true;
+            frame.setSize(width * 10 + 17, height * 10 + 40);
+            setup = true;
+            frame.setTitle("Conway's Game of Life - Press ENTER to Start!");
         }
-    }
-
-    public void MouseClicked(MouseEvent e) {
-        
     }
 
     public void paintComponent(Graphics g) {
         g.setColor(getBackground());
         g.fillRect(0, 0, this.getSize().width, this.getSize().height);
-        if (started) {
+        if (setup) {
             for (int h = 0; h < height; h++) {
                 for (int w = 0; w < width; w++) {
                     if (!grid[h][w]) {
@@ -80,4 +78,46 @@ public class ConwaysGame extends JPanel{
             }
         }
     }
+
+    public void mouseClicked(MouseEvent e) {
+        int gridX = e.getX() / 10;
+        int gridY = e.getY() / 10;
+        if (!grid[gridY][gridX] && gridX <= width && gridY <= height) {
+            grid[gridY][gridX] = true;
+        } else {
+            grid[gridY][gridX] = false;
+        }
+        frame.repaint();
+    }
+
+    public void mousePressed(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {}
+
+    public void mouseExited(MouseEvent e) {}
+
+    public void mouseDragged(MouseEvent e) {
+        int gridX = e.getX() / 10;
+        int gridY = e.getY() / 10;
+        if (!grid[gridY][gridX] && gridX <= width && gridY <= height) {
+            grid[gridY][gridX] = true;
+        } else {
+            grid[gridY][gridX] = false;
+        }
+        frame.repaint();
+    }
+
+    public void mouseMoved(MouseEvent e) {}
+
+    public void keyTyped(KeyEvent e) {
+        if (setup && e.getKeyCode() == 10) {
+
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {}
+
+    public void keyReleased(KeyEvent e) {}
 }
